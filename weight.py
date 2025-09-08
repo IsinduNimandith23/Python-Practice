997d0aa2a9f0c78937ae251f455af7d478713bd1
Isindu Nimandith <isindunimandith23@gmail.com>
2025-09-08 17:56:06 +0530

    weight

---
+++ weight.py
@@ -0,0 +1,13 @@
+unit = input("Enter which unit: kg or pound: ")
+weight = float(input(f"enter weight: "))
+
+if unit == "kg" :
+    result = weight * 2.205
+    print(result)
+elif unit == "pound":
+    result = weight / 2.205
+    print(result)
+else:
+    print("choose a weight")
+
+
